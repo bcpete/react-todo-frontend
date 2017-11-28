0dc7b1d8a9d05ae0b4b586b85cc8db8ac2ad98e3
hometownzero <bradycpeters@gmail.com>
2017-11-27 19:37:29 -0600

    added express server, 404 page bug now fixed.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import RegisterPage from '../components/RegisterPage';
 import Header from '../components/Header';
 import DashBoard from '../components/DashBoard';
@@ -8,17 +8,17 @@ import EditTodoPage from '../components/EditTodoPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router>
     <div>
       <Header />
       <Switch>
         <Route path="/" exact component={DashBoard}/>
         <Route path="/add" component={AddTodoPage} />
         <Route path="/edit/:id" component={EditTodoPage} />
-        <Route component={NotFoundPage} />
+        <Route component={ NotFoundPage } />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
